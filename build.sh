9e106cda34b51debf3547770ae3f035a6db6116f
Chenhao Ye <ye.chenhao.ych@gmail.com>
2024-12-26 14:29:03 -0600

    update build script

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-mkdir -p hugo_extended_0.119.0
-cd hugo_extended_0.119.0
-wget https://github.com/gohugoio/hugo/releases/download/v0.119.0/hugo_extended_0.119.0_darwin-universal.tar.gz
-tar -xvzf hugo_extended_0.119.0_darwin-universal.tar.gz
-cd ..
+if [ ! -f "hugo_extended_0.119.0/hugo" ]; then
+  mkdir -p hugo_extended_0.119.0
+  cd hugo_extended_0.119.0
+  wget https://github.com/gohugoio/hugo/releases/download/v0.119.0/hugo_extended_0.119.0_darwin-universal.tar.gz
+  tar -xvzf hugo_extended_0.119.0_darwin-universal.tar.gz
+  cd ..
+fi
+
 hugo_extended_0.119.0/hugo -D
