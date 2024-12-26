ff77b63a41b68b86347f719cc799dcadac0dc8d0
Chenhao Ye <ye.chenhao.ych@gmail.com>
2024-12-26 12:12:28 -0600

    include script to build with hugo v0.119.0, the last version supported by hugoblox

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+mkdir -p hugo_extended_0.119.0
+cd hugo_extended_0.119.0
+wget https://github.com/gohugoio/hugo/releases/download/v0.119.0/hugo_extended_0.119.0_darwin-universal.tar.gz
+tar -xvzf hugo_extended_0.119.0_darwin-universal.tar.gz
+cd ..
+hugo_extended_0.119.0/hugo -D
